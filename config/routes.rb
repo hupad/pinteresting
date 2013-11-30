814f3948f942dc6d1413d5993be593c63bcc919d
Harish <harish.upad@gmail.com>
2013-11-30 07:23:33 -0500

    Implemented Devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pinteresting::Application.routes.draw do
+  devise_for :users
   root "page#home"
   get "about" => "page#about"
   # The priority is based upon order of creation: first created -> highest priority.
