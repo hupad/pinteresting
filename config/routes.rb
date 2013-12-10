66e0b903c70083181bf5d4341e5b4614e94affba
Harish <harish.upad@gmail.com>
2013-12-10 10:29:09 -0500

    Polished the app a bit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Pinteresting::Application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "page#home"
+  root "pins#index"
   get "about" => "page#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
